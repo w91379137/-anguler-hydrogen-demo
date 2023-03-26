ce5c6f69fe3daa28646784e7933c2f15b462ea70
w91379137 <w913791379137@gmail.com>
2023-03-26 16:45:28 +0800

    [feat] 轉移 ignore-before-finish

--- src/app/page/decorator-ignore-before-finish/decorator-ignore-before-finish.component.ts
+++ src/app/page/decorator-ignore-before-finish/decorator-ignore-before-finish.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IgnoreBeforeFinish } from 'src/app/module/anguler-hydrogen/decorator/ignore-before-finish/ignore-before-finish-decorator';
+import { delay } from 'src/app/module/anguler-hydrogen/util/std-tool';
 
 @Component({
   selector: 'app-decorator-ignore-before-finish',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DecoratorIgnoreBeforeFinishComponent implements OnInit {
 
+  // ====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  // ====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
+
+  @IgnoreBeforeFinish({uuid: 'Test', timeoutms: 90000})
+  async testClick() {
+    console.log('testClick start')
+    await delay(10000)
+    console.log('testClick end')
+  }
+
+  @IgnoreBeforeFinish({uuid: 'Test', timeoutms: 90000})
+  async testClick2() {
+    console.log('testClick2 start')
+    await delay(10000)
+    console.log('testClick2 end')
+  }
 }
--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CheckWait } from 'src/app/module/anguler-hydrogen/decorator/ignore-before-finish/ignore-before-finish-decorator';
-import { delay } from 'src/app/module/anguler-hydrogen/util/std-tool';
 
 @Component({
   selector: 'app-home',
@@ -17,16 +15,4 @@ export class HomeComponent implements OnInit {
   }
 
   // ====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
-
-  @CheckWait({uuid: 'gg', timeoutms: 90000})
-  async testClick() {
-    console.log('testClick')
-    await delay(10000)
-  }
-
-  @CheckWait({uuid: 'gg', timeoutms: 90000})
-  async testClick2() {
-    console.log('testClick2')
-    await delay(10000)
-  }
 }
